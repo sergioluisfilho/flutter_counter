571b17a3eecdc4d318c1bc6cf530c0a4bafa931e
sergioluisfilho <sergioluisfilho@hotmail.com>
2020-07-20 03:06:13 -0300

    second

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,91 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-        title: 'Contador',
-        home: Scaffold(
-            appBar: AppBar(
-              backgroundColor: Colors.deepPurple,
-              title: Center(
-                child: Text('Contador'),
-              ),
-            ),
-            body: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Center(
-                  child: Text(
-                    "Pessoas: 0",
-                    style: TextStyle(
-                        color: Colors.deepPurple,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
-                  ),
+  runApp(MaterialApp(
+    title: 'Contador',
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple,
+        title: Center(
+          child: Text('Contador'),
+        ),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            "images/img.jpeg",
+            fit: BoxFit.fill,
+            height: 1000.0,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Text(
+                  "Pessoas: 0",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Center(
-                        child: FlatButton(
-                          child: Text(
-                            '+1',
-                            style: TextStyle(
-                                fontSize: 40, color: Colors.deepPurple),
-                          ),
-                          onPressed: () {},
-                        ),
-                      ),
-                    ),
-                    Center(
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Center(
                       child: FlatButton(
                         child: Text(
-                          '-1',
+                          '+1',
                           style:
                               TextStyle(fontSize: 40, color: Colors.deepPurple),
                         ),
-                        onPressed: () {},
+                        onPressed: () {
+                          debugPrint('+1');
+                        },
                       ),
                     ),
-                  ],
-                ),
-                Center(
-                  child: Text(
-                    "Pode entrar",
-                    style: TextStyle(
-                        color: Colors.deepPurple,
-                        fontSize: 25,
-                        fontStyle: FontStyle.italic),
                   ),
-                )
-              ],
-            ))),
-  );
+                  Center(
+                    child: FlatButton(
+                      child: Text(
+                        '-1',
+                        style:
+                            TextStyle(fontSize: 40, color: Colors.deepPurple),
+                      ),
+                      onPressed: () {
+                        debugPrint('-1');
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              Center(
+                child: Text(
+                  "Pode entrar",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                      fontStyle: FontStyle.italic),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
